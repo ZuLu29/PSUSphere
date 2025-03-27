f3cf70d622568713027b9652b23ad5c05438296e
ZuLu29 <markneilconde850@gmail.com>
2025-03-27 13:37:30 +0800

    activity 2 finish

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = "home"  # Changed from 'home' to be more descriptive
+    template_name = "home.html"
+
 
 # Create your views here.
